e1f78578434a4a046f3a8772b565a008ab6aae7f
JohnElthon <johnelthon1@gmail.com>
2026-03-17 12:02:04 -0300

    .

---
+++ 2-Fundamentos/Fundamentos/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import FirstComponents from './components/FirstComponents'
+
+function App() {
+  
+
+  return (
+    <>
+      <FirstComponents/>
+    </>
+     )       
+  
+}
+
+export default App;
---
+++ 2-Fundamentos/Fundamentos/src/components/FirstComponents.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FirstComponents = () => {
+  return (
+    <>
+    <div>
+
+        <h1 className='Titulo'>React.js</h1>
+        <p className='Paragrafo'>Senac Santos-Sp</p>
+        
+        <hr />
+
+    </div>
+    <div>
+        <h1 className='Titulo2'>FirstComponents</h1>
+        <p className='Paragrafo'>Primeira aula de fundamentos</p>
+    </div>
+    </>
+  )
+}
+    
+export default FirstComponents
